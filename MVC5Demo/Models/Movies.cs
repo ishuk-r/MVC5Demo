30cadc43793eed51faab9c1e3090984bc01f687d
Ishu <ishukumar724@gmail.com>
2021-03-20 23:52:48 +0530

    Add project files.

---
+++ MVC5Demo/Models/Movies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVC5Demo.Models
+{
+    public class Movies
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext() : base("MovieDbContext")
+        {
+
+        }
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
